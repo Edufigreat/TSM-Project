ecbf774a17c041fac881112ad2bef4dac16ad5d8
Edufigreat <edufiguegro@gmail.com>
2022-06-09 22:01:26 -0500

    Portal

---
+++ Assets/Warrior/FadeOutScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutScript : MonoBehaviour
+{
+    public GameObject LifePoint;
+    public int numberOfLP;
+    public Vector3 offset;
+    public GameObject explosionParticles;
+
+    void Start()
+    {
+    }
+
+    public void Explode(float time)
+    {
+        StartCoroutine(DelayExplosion(time));
+    }
+
+    private IEnumerator DelayExplosion(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SpawnLifePoints();
+        Instantiate(explosionParticles, transform.position + offset, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void SpawnLifePoints()
+    {
+        for (int i = 0; i < numberOfLP; i++)
+        {
+            
+            Instantiate(LifePoint,
+                gameObject.transform.position,
+                gameObject.transform.rotation);
+        }
+
+    }
+}
